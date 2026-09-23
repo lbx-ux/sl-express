02fe365f595bd89a109490f77d114d845091bc57
lbx-ux <2953997419@qq.com>
2026-09-23 14:44:42 +0800

    运费微服务-查询运费模板列表（优化 抽取工具类）

--- sl-express-ms-carriage/sl-express-ms-carriage-service/src/main/java/com/sl/ms/carriage/service/impl/CarriageServiceImpl.java
+++ sl-express-ms-carriage/sl-express-ms-carriage-service/src/main/java/com/sl/ms/carriage/service/impl/CarriageServiceImpl.java
@@ -1,14 +1,13 @@
 package com.sl.ms.carriage.service.impl;
 
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.sl.ms.carriage.domain.dto.CarriageDTO;
 import com.sl.ms.carriage.entity.CarriageEntity;
 import com.sl.ms.carriage.mapper.CarriageMapper;
 import com.sl.ms.carriage.service.CarriageService;
-import com.sl.transport.common.util.BeanUtil;
+import com.sl.ms.carriage.utils.CarriageUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.Collections;
@@ -33,12 +32,7 @@ public class CarriageServiceImpl extends ServiceImpl<CarriageMapper, CarriageEnt
 
         return list.stream()
                 //转化对象，返回集合数据
-                .map(carriageEntity -> {
-                    CarriageDTO carriageDTO = BeanUtil.toBean(carriageEntity, CarriageDTO.class);
-                    //关联城市数据按照逗号分割成集合
-                    carriageDTO.setAssociatedCityList(StrUtil.split(carriageEntity.getAssociatedCity(), ','));
-                    return carriageDTO;
-                })
+                .map(CarriageUtils::toDTO)
                 .collect(Collectors.toList());
     }
 }
