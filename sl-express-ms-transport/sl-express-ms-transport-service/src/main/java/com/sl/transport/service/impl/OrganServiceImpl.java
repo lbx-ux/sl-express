da945a037b35c7de40b0d36b89a956a60f9fecfd
lbx-ux <2953997419@qq.com>
2026-09-26 20:31:01 +0800

    路线规则-机构管理(机构树实现）

--- sl-express-ms-transport/sl-express-ms-transport-service/src/main/java/com/sl/transport/service/impl/OrganServiceImpl.java
+++ sl-express-ms-transport/sl-express-ms-transport-service/src/main/java/com/sl/transport/service/impl/OrganServiceImpl.java
@@ -1,20 +1,29 @@
 package com.sl.transport.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.lang.tree.Tree;
+import cn.hutool.core.lang.tree.TreeUtil;
 import cn.hutool.core.util.ObjectUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sl.transport.common.exception.SLException;
 import com.sl.transport.domain.OrganDTO;
 import com.sl.transport.enums.ExceptionEnum;
 import com.sl.transport.repository.OrganRepository;
 import com.sl.transport.service.OrganService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrganServiceImpl implements OrganService {
     private final OrganRepository organRepository;
+    private final ObjectMapper objectMapper;
 
     //根据id查询
     @Override
@@ -45,6 +54,20 @@ public class OrganServiceImpl implements OrganService {
     //查询机构树
     @Override
     public String findAllTree() {
-        return "";
+        List<OrganDTO> organDTOList = this.findAll(null);
+        if(CollUtil.isEmpty(organDTOList)){
+            return "";
+        }
+        List<Tree<Long>> treeList = TreeUtil.build(organDTOList, 0L, (object, node) -> {
+            node.setId(object.getId());
+            node.setParentId(object.getParentId());
+            node.putAll(BeanUtil.beanToMap(object));
+            node.remove("bid");
+        });
+        try {
+            return this.objectMapper.writeValueAsString(treeList);
+        } catch (JsonProcessingException e) {
+            throw new SLException("序列化json出错！", e);
+        }
     }
 }
